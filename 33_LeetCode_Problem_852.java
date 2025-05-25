8c09ebcf611a7bf69f1672a443823821a504af0b
shivamvishwakarma0000 <ipsshivam8858@gmail.com>
2025-05-25 12:06:20 +0530

    init commit

---
+++ 33_LeetCode_Problem_852.java
@@ -0,0 +1,20 @@
+public class PeakMountain{
+    public static void main(String[] args) {
+         int[] arr={0,1,5,0};
+         int ans=Peak(arr);
+        System.out.println(ans);
+    }
+    static int Peak(int[] arr){
+        int max=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+
+        }
+        return max;
+
+
+    }
+}
